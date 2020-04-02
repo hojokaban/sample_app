2e2fa63ebbd0ed2047d5579d38be8cc36c46c074
hojokaban <hojokaban@gmail.com>
2020-04-02 23:20:58 +0900

    [Finish]sample_app

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07e5ce70fcb202761bc45c5bc1c3088f94adf731c453cd7bf1eab6e187aa184ee44b59120565fe815f3eb940daa108ea040e997ea566bcf93b046f4b8eed5da3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_220933) do
+ActiveRecord::Schema.define(version: 2020_04_02_125644) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
